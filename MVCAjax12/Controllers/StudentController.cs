be347f55fce36915374358502f531d1f1509620c
Lincoln Martinez <lincoln.martinez@tecsup.edu.pe>
2021-11-15 23:36:45 -0500

    Agregar archivos de proyecto.

---
+++ MVCAjax12/Controllers/StudentController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Service;
+using Domain;
+using MVCAjax12.Models;
+
+namespace MVCAjax12.Controllers
+{
+    public class StudentController : Controller
+    {
+        private StudentService service = new StudentService();
+        // GET: Student
+        public ActionResult IndexRazor()
+        {
+            var model = (from c in service.Get()
+                         select new StudentModel
+                         {
+                             ID = c.studentID,
+                             Address = c.studentAddress,
+                             Name = c.studentName
+                         }).ToList();
+
+            return View(model);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getStudent(string id)
+        {
+            return Json(service.Get(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult CreateStudent(Student std)
+        {
+            service.Insert(std);
+            string message = "SUCCESS";
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVCAjax12/Models/StudentModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAjax12.Models
+{
+    public class StudentModel
+    {
+        public int ID { get; set; }
+        public string Address { get; set; }
+        public string Name { get; set; }
+    }
+}
